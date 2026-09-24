4a194457cddef073088779b594f23d955e6df14f
zkz098 <zkz20081204@126.com>
2026-09-24 19:37:34 +0800

    perf(native): enable mimalloc as global allocator

--- crates/limedl-native/src/main.rs
+++ crates/limedl-native/src/main.rs
@@ -1,5 +1,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
 slint::include_modules!();
 
 mod autostart;
